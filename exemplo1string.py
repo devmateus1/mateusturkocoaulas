ebb5c6a9f8d8c9cf6c0080eca7665f13d71694ab
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-10 19:42:45 -0300

    exercicios string

--- exemplo1string.py
+++ exemplo1string.py
@@ -5,3 +5,8 @@ print(nome.title())
 nome = "Ada Lovelace"
 print(nome.upper())
 print(nome.lower())
+
+primeiro_nome = "ada"
+sobrenome = "lovelace"
+nome_completo = primeiro_nome + "" + sobrenome
+print(nome_completo)
