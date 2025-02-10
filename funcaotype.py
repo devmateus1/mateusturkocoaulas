35684750e7010e7719344ec2ac2956fa7ba39a67
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-10 19:24:37 -0300

    atividade funcoes type

---
+++ funcaotype.py
@@ -0,0 +1,11 @@
+faturamento = 1000
+print('Para variavel faturamento que recebeu 1000 o tipo é', type(faturamento))
+
+faturamento = 1000.00
+print('Para variavel faturamento que recebeu 1000.00 o tipo é', type(faturamento))
+
+faturamento = '1.000'
+print('Para variavel faturamento que recebeu "1.000" o tipo é', type(faturamento))
+
+ganha_bonus = True
+print('Para variavel ganha_bonus que recebeu "True" o tipo é: '), type(ganha_bonus)
