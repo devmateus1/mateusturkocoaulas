6a9993df8d46443edff7a080cc5de440ae024506
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-10 21:09:49 -0300

    formats e strings

--- StringFormat.py
+++ StringFormat.py
@@ -8,3 +8,13 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 print("O faturamento da loja foi de : {}. O custo foi de: {}. E o lucro foi de: {}. ".format (faturamento, custo, lucro))
+
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+print("O faturamento da loja foi de : (0). O custo foi de: (1). E o lucro foi de: { (2). ".format (faturamento, custo, lucro))
+
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+print("O faturamento da loja foi de : (2). O custo foi de: (1). E o lucro foi de: { (0). ".format (faturamento, custo, lucro))
