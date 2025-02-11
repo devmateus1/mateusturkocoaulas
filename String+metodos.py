cedcc7b48f3ed20e941932e4d1acacb010524221
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-10 22:05:26 -0300

    metodos

--- String+metodos.py
+++ String+metodos.py
@@ -17,4 +17,40 @@ print(nome.count ('a') , "\n")
 #RETORNA true (verdadeiro) OU false (falso) PARA UM TESTE se A STRING TERMINA COM UMA STRING ESPECIFICA
 
 nome = "mateusdecamposturkoco@gmail.com"
-print(nome.enswith ('gmail.com') , "\n")
+print(nome.endswith ('gmail.com'), "\n")
+
+#ENCONTRA A POSIÇÃO DO TERMO PROCURADO, LEMBRE-SE COMEÇA DO ZERO
+nome = "mateusdecamposturkoco@gmail.com"
+print(nome.find ('@') , "\n")
+
+#VERIFICA SE O TEXTO É todo FEITO COM LETRAS
+
+nome = "mateus"
+
+print(nome.isalpha() , "\n")
+
+#VERIFICA SE O TEXTO É FEITO COM NUMEROS.
+
+nome = "123"
+print(nome.isnumeric() , "\n")
+
+#SUBSTITUI UM CARACTERE ESCOLHIDO POR OUTRO.
+nome = "Mateus"
+print(nome.replace ('o', 'u'), "\n")
+
+#SEPARA O TEXTO STRING EM BASEADO EM ALGUM CARACTERE INDICADO 
+nome = "mateus @ decampos turkoco"
+print(nome.split ('@') , "\n")
+
+#COLOCA TODAS AS LETRAS INICIAIS EM MAIUSCULA.
+nome = "mateus de campos turkoco"
+print(nome.title() , "\n")
+
+#RETIRA OS CARACTERES INDESEJADOS, COMO POR EXEMPLO ESPAÇOS QUE NAO AGREGAM VALOR.
+nome = " mateus de campos turkoco "
+print(nome.strip() , "\n")
+
+#RETORNA TRUE OU FALSE PARA UM TESTE DE UMA STRING SE INICIA COM UMA TEXTO ESPECIFICO.
+nome = "mateus 2008"
+print(nome.startswith("ser") , "\n")
+print(nome.startswith("Ser") , "\n")
