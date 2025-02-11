37a87b4206addf8f3a63f57fcbe4821a9f655ad1
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-10 22:16:40 -0300

    exercicios if

---
+++ exemploif.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite um número inteiro: "))
+if num1 > 10:
+    print("Ele é maior que 10")
+if num1 < 10:
+    print("Ele é menor que 10")
+else:
+    print("Ele é igual a 10")        
+
+
+ 
