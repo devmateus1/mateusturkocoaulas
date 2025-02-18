ae88065858e56b86d2236462eaab14aa15284da7
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-18 20:42:59 -0300

    exes

---
+++ exemplo3forenumerate.py
@@ -0,0 +1,4 @@
+funcionarios = ['marta', 'josé','antonio', 'joão', 'francisco', 'ana', 'trobosvaldo']
+for i, funcionarios in enumerate(funcionarios):
+    print('{} é  o funcionario {}'.format(i, funcionarios)):
+    
