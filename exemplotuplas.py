b0c839a9a33776b10e633b5b709a0190a95321be
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-19 19:18:36 -0300

    Create exemplotuplas.py

---
+++ exemplotuplas.py
@@ -0,0 +1,7 @@
+vendas = ('Mateus', '21/05/2030', '21/09/2008', 7000, 'Eng. Software')
+nome, data_contratacao, data_nascimento, salario, cargo = vendas
+print(nome)
+print(data_contratacao)
+print(data_nascimento)
+print(salario)
+print(cargo)
