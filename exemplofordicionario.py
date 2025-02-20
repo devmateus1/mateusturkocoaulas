e8d86361beef2cfd388c3847f18923a08a0173ae
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-20 20:22:25 -0300

    exes

---
+++ exemplofordicionario.py
@@ -0,0 +1,4 @@
+vendas_tecnologia = {'iphone':15000,'samsung galaxy':12000,'Tv Samsung':10000, 'ps5':14300,'tablet':1720, 'ipad':1200}
+
+for chave in vendas_tecnologia:
+    print('O produto {} vendeu {} unidades '.format(chave, vendas_tecnologia[chave]))
---
+++ exemplounpacking.py
@@ -0,0 +1,4 @@
+vendas_tecnologia = {'iphone':15000,'samsung galaxy':12000,'Tv Samsung':10000, 'ps5':14300,'tablet':1720, 'ipad':1200}
+
+for produto, vendas in vendas_tecnologia.items():
+    print('{}: {} definidades:.'.format(produto, vendas))
