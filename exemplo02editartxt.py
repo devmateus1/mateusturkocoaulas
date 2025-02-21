e650f258069141253b18f21b4124d77c34b57aa4
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-21 19:51:59 -0300

    exes

---
+++ exemplo02editartxt.py
@@ -0,0 +1,27 @@
+arquivo2 = open('C:/Users/mateus_turkoco/Documents/txt/GOT.txt', 'w')
+arquivo2.write('Escrevendo no arquivo\n')
+arquivo2.write('John Snow\n')
+arquivo2.write('Arya Stark\n')
+arquivo2.write('Sansa Stark\n')
+arquivo2.write('Bran Stark\n')
+arquivo2.close()
+print("Fim programa")
+
+arquivo3 = open('C:/Users/mateus_turkoco/Documents/txt/Listadecompra.txt', 'w')
+arquivo3.write('Escrevendo no arquivo\n')
+arquivo3.write('Arroz\n')
+arquivo3.write('Feijão\n')
+arquivo3.write('Carne\n')
+arquivo3.write('Batata\n')
+arquivo3.close()
+print("Fim programa")
+
+#r = ler
+#w = escrever
+#
+
+with open('C:/Users/mateus_turkoco/Documents/txt/Resumo 3.txt', 'w') as arquivo4:
+    arquivo4.write('Escrevendo no arquivo\n')
+    arquivo4.write('Neymar\n')
+print("FIm programa")
+
---
+++ leituratxt.py
@@ -0,0 +1,3 @@
+arquivo = open('C:/Users/mateus_turkoco/Documents/txt/Alunos.txt','r')
+linhas = arquivo.readlines()
+print(linhas)
